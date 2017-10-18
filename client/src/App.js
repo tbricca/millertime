596991c93ed679028ba6b1b479d7539a30e67a49
tbricca <tbricca@gmail.com>
2017-10-17 20:24:09 -0700

    here you go reggie

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import JobList from './JobList.js';
 import JobDetail from './JobDetail.js';
 import UserDetail from './UserDetail.js';
-//import Headline from './Headline.js';
+import Headline from './Headline.js';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -43,7 +43,7 @@ class App extends Component {
               </ul>
               <ul className="nav navbar-nav pull-right">
                 <li className="dropdown">
-                  <a href="#" id="nbAcctDD" className="dropdown-toggle" data-toggle="dropdown"><i className="icon-user"></i>Username<i className="icon-sort-down"></i></a>
+                  <a href="#" id="nbAcctDD" className="dropdown-toggle" data-toggle="dropdown"><i className="icon-user"></i> < Headline /> The Magnicent<i className="icon-sort-down"></i></a>
                   <ul className="dropdown-menu pull-right">
                     <li><a href="#">Log Out</a></li>
                   </ul>
@@ -53,10 +53,13 @@ class App extends Component {
           </div>
           <div id="wrapper">
             <div id="sidebar-wrapper" className="col-md-2">
-                <div id="sidebar">
-                    <JobList activeJob={this.state.activeJob}/>
+              
+                <div id="headlineDiv">
+                  <h4> < Headline />'s Job List </h4>
                 </div>
-              </div>
+                  <JobList activeJob={this.state.activeJob}/>
+              
+            </div>
               <div id="main-wrapper" className="col-md-10 pull-right">
                 <div id="main">
                   <div className="page-header">
@@ -66,7 +69,7 @@ class App extends Component {
                   <Route exact path="/userDetailRoute" component={UserDetail} />
                 </div>
                 <div className="col-md-12 footer">
-                  <ul className="nav navbar-nav">A Project Inspired by Beth Miller, Cooked Up By: Chad Coker, Brian Conner, Reggie Baker & Tony Bricca</ul>
+                  <ul className="nav navbar-nav">A Project Inspired by Beth Miller, Cooked Up By: Chad Coker, Brian Conner, Reggie Baker and Tony Bricca</ul>
                 </div>
               </div>
           </div>
